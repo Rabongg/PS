f0f3c35b34f11b1bf032d7bb81d637319c6fd915
HEESOO YANG <didgmltn7769@naver.com>
2022-04-26 23:43:13 +0900

    [ADD] simple 삽입 정렬

---
+++ boj/정렬/Selection_sort/#23881.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#define MAX 10000
+using namespace std;
+int arr[MAX];
+
+void swap(int i, int j) {
+  int tmp;
+  tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
+}
+
+int main() {
+  int N, K;
+  
+  cin >> N >> K;
+
+  for(int i=0; i<N; i++) {
+    cin >> arr[i];
+  }
+
+  for(int i=N-1; 0<i; i--) {
+    int max_val = i;
+    for(int j=i-1; 0<=j; j--) {
+      if(arr[max_val] < arr[j]) {
+        max_val = j;
+      }
+    }
+    if (max_val != i) {
+      swap(i, max_val);
+      if(--K == 0) {
+        cout << arr[max_val] << " " << arr[i];
+        return 0;
+      }
+    }
+  }
+
+  cout << -1;
+}
